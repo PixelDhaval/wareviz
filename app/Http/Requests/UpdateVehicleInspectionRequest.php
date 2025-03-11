7c29bb1224501db39ad451f48657c06c5f3542ed
dhaval <dhavalmaru0802@gmail.com>
2025-03-11 15:56:38 +0530

    d

--- app/Http/Requests/UpdateVehicleInspectionRequest.php
+++ app/Http/Requests/UpdateVehicleInspectionRequest.php
@@ -26,7 +26,7 @@ class UpdateVehicleInspectionRequest extends FormRequest
             "inspection_no" => "sometimes|nullable|string",
             "inspection_date" => "sometimes|nullable|date",
             "inspection_result" => "sometimes|nullable|string",
-            "remarks" => "sometimes|nullable|string",
+            "remark" => "sometimes|nullable|string",
             "inspection_type" => "sometimes|nullable|in:SGS,3rd Party,Agency,Shipper,Consignee,Self",
             "inspection_by" => "sometimes|nullable|string"
         ];
